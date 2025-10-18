1025c8e25aaadcedf0a9428dd86e6838e6cbfb2b
Kaduh15 <kadu.silva2014@gmail.com>
2025-10-18 09:36:33 -0300

    style: melhora componentes visuais e UX
    - Ajusta componentes header e cards para melhor layout
    - Melhora responsividade dos elementos visuais
    - Atualiza estilos CSS globais para maior consistencia
    - Refina template de registro com melhor UX
    - Aplica melhorias visuais gerais na interface

--- web/src/app/components/card-pokemon/card-pokemon.ts
+++ web/src/app/components/card-pokemon/card-pokemon.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Heart, LucideAngularModule, Plus, X } from 'lucide-angular';
 
 import { PokemonType } from '../../../utils/pokemon-type.util';
+import { Api } from '../../services/api';
 import { TypePokemonBadge } from "../type-pokemon-badge/type-pokemon-badge";
 
 @Component({
@@ -20,4 +21,28 @@ export class CardPokemon {
   @Input() imageUrl!: string;
   @Input() isTeam: boolean = false;
   @Input() isFavorite: boolean = false;
+
+  apiService = inject(Api);
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+    console.log(`Pokémon ${this.name} favorite status: ${this.isFavorite}`);
+
+    if (this.isFavorite) {
+      this.apiService.addFavoritePokemon(this.id).subscribe({
+        next: (res) => {
+          console.log('Favorite added:', res);
+        },
+        error: (err) => {
+          console.error('Failed to add favorite:', err);
+          this.isFavorite = !this.isFavorite;
+        }
+      });
+    }
+  }
+
+  toggleTeam() {
+    this.isTeam = !this.isTeam;
+    console.log(`Pokémon ${this.name} team status: ${this.isTeam}`);
+  }
 }
